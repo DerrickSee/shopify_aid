469f4f55abb423032a44fbba7f660ca68369d7cf
Derrick See <dsee@jenniferfurniture.com>
2016-03-15 02:16:09 -0400

    Enhanced match prices

--- hello/management/commands/match_prices.py
+++ hello/management/commands/match_prices.py
@@ -14,48 +14,43 @@ from hello.models import *
 class Command(BaseCommand):
     help = 'Match vendor products to products'
 
-    def add_arguments(self, parser):
-        parser.add_argument('vendor', nargs='+', type=str)
-
-
     def handle(self, *args, **options):
         upholstery = ProductType.objects.filter(title__in=('sofas', 'loveseats', 'accent chairs', 'sofa chairs', 'daybeds', 'living room sets', 'ottomans', 'recliners', 'sectionals', 'sofa chaises'))
         vendors = Vendor.objects.filter(title__in=('Ashley', 'United', 'Jonathan Louis', 'Istikbal'))
-        vendor, created = Vendor.objects.get_or_create(title=options['vendor'][0])
-        print vendor
-        for product in Product.objects.filter(vendor=vendor):
-            skus = product.sku.split('+')
-            cost_price = Decimal('0.00')
-            all_accounted = True
-            for sku in skus:
-                sku = sku.split('*')
-                vp = get_object_or_None(VendorProduct, vendor=vendor, sku=sku[0])
-                try:
-                    qty = sku[1]
-                except:
-                    qty = '1'
-                if vp:
-                    cost_price += vp.price * Decimal(qty)
-                else:
-                    all_accounted = False
-            if all_accounted:
-                product.cost_price = cost_price
-                if product.product_type in upholstery and product.vendor in vendors:
-                    product.retail_price = cost_price * Decimal('3.57')
-                else:
-                    product.retail_price = cost_price * Decimal('3.25')
-                sale_price = product.retail_price * Decimal('0.7')
-                if sale_price <= 200:
-                    sale_price = math.ceil(sale_price) - 0.01
-                else:
-                    if sale_price % 100  < 50:
-                        sale_price = math.floor(sale_price / 100) * 100 + 49.99
+        for vendor in Vendor.objects.all():
+            for product in Product.objects.filter(vendor=vendor):
+                skus = product.sku.split('+')
+                cost_price = Decimal('0.00')
+                all_accounted = True
+                for sku in skus:
+                    sku = sku.split('*')
+                    vp = get_object_or_None(VendorProduct, vendor=vendor, sku=sku[0])
+                    try:
+                        qty = sku[1]
+                    except:
+                        qty = '1'
+                    if vp:
+                        cost_price += vp.price * Decimal(qty)
                     else:
-                        sale_price = math.ceil(sale_price / 100) * 100 - 0.01
-                product.sale_price = sale_price
-                product.save()
-                print '%s updated' % product.sku
-            else:
-                print '%s skipped' % product.sku
+                        all_accounted = False
+                if all_accounted:
+                    product.cost_price = cost_price
+                    if product.product_type in upholstery and product.vendor in vendors:
+                        product.retail_price = cost_price * Decimal('3.57')
+                    else:
+                        product.retail_price = cost_price * Decimal('3.25')
+                    sale_price = product.retail_price * Decimal('0.7')
+                    if sale_price <= 200:
+                        sale_price = math.ceil(sale_price) - 0.01
+                    else:
+                        if sale_price % 100  < 50:
+                            sale_price = math.floor(sale_price / 100) * 100 + 49.99
+                        else:
+                            sale_price = math.ceil(sale_price / 100) * 100 - 0.01
+                    product.sale_price = sale_price
+                    product.save()
+                    print '%s updated' % product.sku
+                else:
+                    print '%s skipped' % product.sku
 
         print 'Price Matched'
