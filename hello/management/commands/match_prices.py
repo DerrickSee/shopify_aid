4ffc770b1c4493239ae4dfc0ab63be3195d2b38e
derrick <derrick.see@thatgreenspace.com.sg>
2016-03-24 22:23:44 -0400

    Match price now update retail price to .99

--- hello/management/commands/match_prices.py
+++ hello/management/commands/match_prices.py
@@ -43,13 +43,14 @@ class Command(BaseCommand):
                 if all_accounted:
                     product.cost_price = cost_price
                     if product.product_type in upholstery and product.vendor in vendors:
-                        product.retail_price = cost_price * Decimal('3.57')
+                        retail_price = cost_price * Decimal('3.57')
                     else:
-                        product.retail_price = cost_price * Decimal('3.25')
+                        retail_price = cost_price * Decimal('3.25')
                     if product.product_type in mattresses:
-                        sale_price = product.retail_price * Decimal('0.5')
+                        sale_price = retail_price * Decimal('0.5')
                     else:
-                        sale_price = product.retail_price * Decimal('0.7')
+                        sale_price = retail_price * Decimal('0.7')
+                    retail_price = math.ceil(retail_price / 10) * 10 - 0.01
                     sale_price = math.ceil(sale_price / 10) * 10 - 0.01
                     # if sale_price <= 200:
                     #     sale_price = math.ceil(sale_price) - 0.01
@@ -58,6 +59,7 @@ class Command(BaseCommand):
                     #         sale_price = math.floor(sale_price / 100) * 100 + 49.99
                     #     else:
                     #         sale_price = math.ceil(sale_price / 100) * 100 - 0.01
+                    product.retail_price = retail_price
                     product.sale_price = sale_price
                     product.save()
                     print '%s updated' % product.sku
